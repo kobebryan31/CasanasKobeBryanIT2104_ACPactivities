58382d6f2fdf369278788444adde5cc3225ac117
kobebryan31 <casanaskobe@gmail.com>
2024-08-21 12:03:14 +0800

    Add files via upload

---
+++ tempconvert.py
@@ -0,0 +1,3 @@
+celsius = float(input("Enter a celsius value:"))
+fahrenheit = (celsius * 9/5) + 32
+print(f"The temperature in Fahreinheit is: {fahrenheit:.2f}Fahrenheit")
---
+++ welcome.py
@@ -0,0 +1,3 @@
+print("Welcome to CS 121: Advanced Computer Programming")
+name = input("Please enter your name: ")
+print(name + ", This course will be fun")
